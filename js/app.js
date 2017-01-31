aeb393c8801feb12c7fa6dfd471a90f143287979
williamd <willy1divx@yahoo.com>
2017-01-31 21:10:29 +0000

    inital file commit

---
+++ js/app.js
@@ -0,0 +1,141 @@
+
+var solarSystem = {
+	 venus : {
+	 	image: "img/venus.gif",
+	 	information: "Venus and Earth are similar in size, mass, density, composition, and distance from the sun. There, however, is where the similarities end.Venus is covered by a thick, rapidly spinning atmosphere, creating a scorched world with temperatures hot enough to melt lead and a surface pressure 90 times that of Earth. Because of its proximity to Earth and the way its clouds reflect sunlight, Venus appears to be the brightest planet in the sky. Like Mercury, Venus can be seen periodically passing across the face of the sun. These transits occur in pairs, with more than a century separating each pair. Since the telescope was invented, transits have been observed in 1631, 1639; 1761, 1769; and 1874, 1882. On June 8, 2004, astronomers worldwide saw the tiny dot of Venus crawl across the sun; the second in this pair of early 21st-century transits will occur June 6, 2012.",
+	 	diameter:"Diameter\:	4,879 km", 
+	 	mass:"Mass\:	3.30 x 10^23 kg (5.5% Earth)",
+	 	moons:"Moons\:	None",
+	 	orbitDistance: "Orbit Distance\:	108,209,475 km (0.73 AU)",
+	 	orbitPeriod:"Orbit Period\:	225 days",
+	 	temp:"Surface Temperature\:	462 °C",
+	 	firstRecord:"First Record\:	17th century BC"
+	 	 },
+	 mercury : {
+	 	image: "img/mercury.gif",
+	 	information: "Mercury is the closest planet to the Sun and due to its proximity it is not easily seen except during twilight. For every two orbits of the Sun, Mercury completes three rotations about its axis and up until 1965 it was thought that the same side of Mercury constantly faced the Sun. Thirteen times a century Mercury can be observed from the Earth passing across the face of the Sun in an event called a transit, the next will occur on the 9th May 2016.",
+	 	diameter:"Diameter\:	12,104 km", 
+	 	mass:"Mass\:	4.87 x 10^24 kg (81.5% Earth)",
+	 	moons:"Moons\:	None",
+	 	orbitDistance: "Orbit Distance\:	57,909,227 km (0.39 AU)",
+	 	orbitPeriod:"Orbit Period\:	88 days",
+	 	temp:"Surface Temperature\:	-173 to 427°C",
+	 	firstRecord:"First Record\:	14th century BC"
+	 	 },
+	 earth : {
+	 	image: "img/earth.gif",
+	 	information: "Earth, our home planet, is the only planet in our solar system known to harbor life. All of the things we need to survive are provided under a thin layer of atmosphere that separates us from the uninhabitable void of space. Earth is made up of complex, interactive systems that are often unpredictable. Air, water, land, and life—including humans—combine forces to create a constantly changing world that we are striving to understand.",
+	 	diameter:"Equatorial Diameter\:	12,756 km", 
+	 	mass:"Mass\:	5.97 x 10^24 kg",
+	 	moons:"Moons\:	1 (The Moon)",
+	 	orbitDistance: "Orbit Distance\:	149,598,262 km (1 AU)",
+	 	orbitPeriod:"Orbit Period\:	365.26 days",
+	 	temp:"Surface Temperature\:	-88 to 58°C",
+	 	firstRecord:"Adam and Eve"
+	 	 },
+	 mars : {
+	 	image: "img/mars.gif",
+	 	information: "Mars is the fourth planet from the Sun and is the second smallest planet in the solar system. Named after the Roman god of war, Mars is also often described as the “Red Planet” due to its reddish appearance. Mars is a terrestrial planet with a thin atmosphere composed primarily of carbon dioxide.",
+	 	diameter:"Equatorial Diameter:	6,792 km", 
+	 	mass:"Mass\:	6.42 x 10^23 kg (10.7% Earth)",
+	 	moons:"Moons\:	2 (Phobos & Deimos)",
+	 	orbitDistance: "Orbit Distance\:	227,943,824 km (1.52 AU)",
+	 	orbitPeriod:"Orbit Period\:	687 days (1.9 years)",
+	 	temp:"Surface Temperature\:	-153 to 20 °C",
+	 	firstRecord:"First Record\:	2nd millennium BC"
+	 	 },
+	 jupiter : {
+	 	image: "img/jupiter.gif",
+	 	information: "The planet Jupiter is the fifth planet out from the Sun, and is two and a half times more massive than all the other planets in the solar system combined. It is made primarily of gases and is therefore known as a \“gas giant\”.",
+	 	diameter:"Equatorial Diameter\:	142,984 km", 
+	 	mass:"Mass\:	1.90 × 10^27 kg (318 Earths)",
+	 	moons:"Moons\:	67 (Io, Europa, Ganymede & Callisto)",
+	 	orbitDistance: "Orbit Distance\:	778,340,821 km (5.20 AU)",
+	 	orbitPeriod:"Orbit Period\:	4,333 days (11.9 years)",
+	 	temp:"Effective Temperature\:	-148 °C",
+	 	firstRecord:"First Record\:	7th or 8th century BC"
+	 	 },
+	 saturn : {
+	 	image: "img/saturn.gif",
+	 	information: "Saturn is the sixth planet from the Sun and the most distant that can be seen with the naked eye. Saturn is the second largest planet and is best known for its fabulous ring system that was first observed in 1610 by the astronomer Galileo Galilei. Like Jupiter, Saturn is a gas giant and is composed of similar gasses including hydrogen, helium and methane.",
+	 	diameter:"Equatorial Diameter\:	120,536 km", 
+	 	mass:"Mass\:	5.68 × 10^26 kg (95 Earths)",
+	 	moons:"Moons\:	62 (Titan, Enceladus, Iapetus & Rhea)",
+	 	orbitDistance: "Orbit Distance\:	1,426,666,422 km (9.54 AU)",
+	 	orbitPeriod:"Orbit Period\:	10,756 days (29.5 years)",
+	 	temp:"Effective Temperature\:	-178 °C",
+	 	firstRecord:"First Record\:	8th century BC"
+	 	 },
+	 uranus : {
+	 	image: "img/uranus.gif",
+	 	information: "Uranus is the seventh planet from the Sun. It’s not visible to the naked eye, and became the first planet discovered with the use of a telescope. Uranus is tipped over on its side with an axial tilt of 98 degrees. It is often described as \“rolling around the Sun on its side.\”",
+	 	diameter:"Equatorial Diameter\:	51,118 km", 
+	 	mass:"Mass\:	8.68 × 10^25 kg (15 Earths)",
+	 	moons:"Moons\:	27 (Miranda, Titania, Ariel, Umbriel & Oberon)",
+	 	orbitDistance: "Orbit Distance\:	2,870,658,186 km (19.19 AU)",
+	 	orbitPeriod:"Orbit Period\:	30,687 days (84.0 years)",
+	 	temp:"Effective Temperature\:	-216 °C",
+	 	firstRecord:"Discovery Date\:	March 13th 1781"
+	 	 },
+	 neptune : {
+	 	image: "img/neptune.gif",
+	 	information: "Neptune is the eighth planet from the Sun making it the most distant in the solar system. This gas giant planet may have formed much closer to the Sun in early solar system history before migrating to its present position.",
+	 	diameter:"Equatorial Diameter\:	49,528 km", 
+	 	mass:"Mass\:	1.02 × 10^26 kg (17 Earths)",
+	 	moons:"Moons\:	14 (Triton)",
+	 	orbitDistance: "Orbit Distance\:	4,498,396,441 km (30.10 AU)",
+	 	orbitPeriod:"Orbit Period\:	60,190 days (164.8 years)",
+	 	temp:"Effective Temperature\:	-214 °C",
+	 	firstRecord:"September 23rd 1846"
+	 	 },
+	 pluto : {
+	 	image: "img/pluto.gif",
+	 	information: "Discovered in 1930, Pluto is the second closest dwarf planet to the Sun and was at one point classified as the ninth planet. Pluto is the largest dwarf planet but only the second most massive, with Eris being the most massive.",
+	 	diameter:"Diameter\:	2,372 km", 
+	 	mass:"Mass\:	1.31 × 10^22 kg (0.17 Moons)",
+	 	moons:"Moons\:	5 (Charon)",
+	 	orbitDistance: "Orbit Distance\:	5,874,000,000 km (39.26 AU)",
+	 	orbitPeriod:"Orbit Period\:	246.0 years",
+	 	temp:"Surface Temperature\:	-229°C",
+	 	firstRecord:"Discovery Date\:	February 18th 1930"
+	 	 }
+};
+
+function getAnswer(){
+		planets = document.getElementById('planet').value;
+		planets = planets.toLowerCase(); 
+		console.log(planets);
+		if (solarSystem[planets] === undefined){
+			$("#image").attr("src","img/jesus.jpg");
+			document.getElementById('message').innerHTML = "YOU'RE NOT SUPPOSED TO BE HERE";
+			document.getElementById('diameter').innerHTML = " ";
+			document.getElementById('funFacts').innerHTML = " ";
+			document.getElementById('mass').innerHTML =  " ";
+			document.getElementById('moons').innerHTML =  " ";
+			document.getElementById('orbitDistance').innerHTML =  " ";
+			document.getElementById('orbitPeriod').innerHTML =  " ";
+			document.getElementById('temp').innerHTML =  " ";
+			document.getElementById('firstRecord').innerHTML =  " ";
+		} 
+		if (planets == 'neptune' || planets == 'mercury' ){
+		 $("#background").attr("background", "img/zodiac.jpg");
+	  } else {
+	  	$("#background").attr("background", "none");
+	  }
+		if (solarSystem[planets] !== undefined){
+		    $("#image").attr("src", solarSystem[planets].image);
+		    document.getElementById('message').innerHTML = solarSystem[planets].information;
+			document.getElementById('diameter').innerHTML = solarSystem[planets].diameter;
+			document.getElementById('funFacts').innerHTML = "FUN FACTS";
+			document.getElementById('mass').innerHTML = solarSystem[planets].mass;
+			document.getElementById('moons').innerHTML = solarSystem[planets].moons;
+			document.getElementById('orbitDistance').innerHTML = solarSystem[planets].orbitDistance;
+			document.getElementById('orbitPeriod').innerHTML = solarSystem[planets].orbitPeriod;
+			document.getElementById('temp').innerHTML = solarSystem[planets].temp;
+			document.getElementById('firstRecord').innerHTML = solarSystem[planets].firstRecord;
+		}
+		if (planets == true){
+		 $("#blackCover").attr("background-color", "none");
+	  } 
+
+}
